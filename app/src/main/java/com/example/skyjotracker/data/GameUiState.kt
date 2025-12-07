09235314a7af8c7019b6b14887d8cf2fb6878816
Adrian Lösch <loeschadrianp+git@gmail.com>
2025-12-07 21:06:22 +0200

    Add documentation for Gamestate

--- app/src/main/java/com/example/skyjotracker/data/GameUiState.kt
+++ app/src/main/java/com/example/skyjotracker/data/GameUiState.kt
@@ -1,10 +1,12 @@
 package com.example.skyjotracker.data
 
-
 data class GameUiState(
+    // Map of player id to player name
     val playerNames: Map<Int, String> = mapOf(),
     val currentRound: Int = 1,
+    // Map of round number to map of player id to score
     val scoreSheet: Map<Int, Map<Int, Int>> = mapOf(),
+    // Map of player id to total score
     val totalScores: Map<Int, Int> = mapOf(),
     val numberOfRounds: Int? = null
 )
